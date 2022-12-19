f00bafc44296d6736b31c6b59ef8d8d2260df069
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-12-19 18:21:57 +0100

    Add navigation and profile page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import MyProfile from './components/MyProfile';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/profile" element={<MyProfile />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/MyProfile.js
@@ -0,0 +1,16 @@
+import '../styles/MyProfile.css';
+
+// eslint-disable-next-line arrow-body-style
+const MyProfile = () => {
+  return (
+    <div className="profile">
+      <div className="missions">
+        <h2>My Messions</h2>
+      </div>
+      <div className="rockets">
+        <h2>My Rockets</h2>
+      </div>
+    </div>
+  );
+};
+export default MyProfile;

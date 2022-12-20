b6497087bac0a600c55585bd7b95f1d9a2761ffb
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2022-12-20 12:21:46 +0300

    add RocketSlice

--- src/redux/Rockets/Rockets.js
+++ src/redux/Rockets/Rockets.js
@@ -1,10 +1,31 @@
-const initialState = [];
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-const rocketsReduser = (state = initialState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
+const initialState = {
+  loading: false,
+  rockets: [],
+  error: null,
 };
+export const getRockets = createAsyncThunk('rockets/fetchRockets', () => fetch('https://api.spacexdata.com/v3/rockets').then((data) => data.json()));
 
-export default rocketsReduser;
+const RocketSlice = createSlice({
+  name: 'rocket',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getRockets.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getRockets.fulfilled, (state, actions) => {
+      state.loading = false;
+      state.rockets = actions.payload;
+      state.error = null;
+    });
+    builder.addCase(getRockets.rejected, (state, actions) => {
+      state.loading = false;
+      state.rockets = [];
+      state.error = actions.error.message;
+    });
+  },
+});
+
+export default RocketSlice.reducer;

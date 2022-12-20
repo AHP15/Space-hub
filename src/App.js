463c85d438b0bc9767498c64874d018ef67648a5
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2022-12-20 08:49:44 +0300

    basic structure for Rockets - Setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
 import MyProfile from './components/MyProfile';
+import Rockets from './components/Rockets';
 import NavBar from './components/NavBar';
+import Store from './redux/SpaceStore';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="container">
-        <Routes>
-          <Route path="/profile" element={<MyProfile />} />
-        </Routes>
+    <Provider store={Store}>
+      <div className="App">
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Rockets />} />
+            <Route path="/profile" element={<MyProfile />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
---
+++ src/components/Rockets.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Rockets.css';
+import Rocket from './Rocket';
+
+const Rockets = () => (
+  <section className="rockets">
+    <div className="container" />
+    <Rocket />
+    <Rocket />
+    <Rocket />
+  </section>
+);
+
+export default Rockets;
